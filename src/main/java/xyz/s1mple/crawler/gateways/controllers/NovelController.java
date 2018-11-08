801573c4f7bca977ef926388d686c8862ed5b1ee
s1mple <798342418@qq.com>
2018-11-08 10:07:09 +0800

    #web-crawler - s1mple - implement novel download feature

--- src/main/java/xyz/s1mple/crawler/gateways/controllers/NovelController.java
+++ src/main/java/xyz/s1mple/crawler/gateways/controllers/NovelController.java
@@ -1,12 +1,17 @@
 package xyz.s1mple.crawler.gateways.controllers;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.s1mple.crawler.application.NovelService;
+import xyz.s1mple.crawler.domain.novel.Novel;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -14,8 +19,14 @@ public class NovelController {
     @Resource
     private NovelService novelService;
 
-    @GetMapping("/novel")
-    public String download(@RequestParam String index) throws InterruptedException, ExecutionException, IOException {
-        return novelService.crawl(index);
+    @GetMapping("/download")
+    public void download(@RequestParam String index, HttpServletResponse response) throws InterruptedException, ExecutionException, IOException {
+        Novel novel = novelService.crawl(index);
+
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode(novel.title() + ".txt", "UTF-8"));
+        IOUtils.copy(new ByteArrayInputStream(novel.contents().getBytes()), response.getOutputStream());
+        response.flushBuffer();
     }
 }
